ac327cae0b283835a1060da40b285501089831fb
wvbe <wybe@x-54.com>
2018-01-22 17:37:32 +0100

    Support namespaced elements and attributes + tests

--- index.js
+++ index.js
@@ -9,23 +9,83 @@ function createHandler () {
 	let dom = doc;
 	let cdata = null;
 
+	const namespaces = [{ '': null }];
+	let currentNamespaces = { '': null };
+
+	function pushAccumulatedNamespaceInfo (node) {
+		const localNamespaces = Object.keys(node.attributes)
+			.filter(name => name.indexOf('xmlns') === 0);
+
+		if (!localNamespaces.length) {
+			// No new namespace info discovered, so exit without doing much else
+			namespaces.push(null);
+			return;
+		}
+
+		// Reduce array to a prefix-to-url mapping
+		const localNamespaceMapping = localNamespaces
+			.reduce((accum, name) => Object.assign(accum, {
+				[name.split(':')[1] || '']: node.attributes[name]
+			}), {});
+
+		// Push to array so the entire namespace info can be popped and recalculated entirely
+		namespaces.push(localNamespaceMapping);
+
+		// Assign the local namespace mapping onto what we already knew
+		currentNamespaces = Object.assign(currentNamespaces, localNamespaceMapping);
+	}
+
+	function popAccumulatedNamespaceInfo () {
+		if (!namespaces.pop()) {
+			// The namespace info for the level that is popped was empty, so exit early
+			return;
+		}
+
+		// Recalculate the (subset) portion of known namespace information
+		currentNamespaces = namespaces.reduce((accum, ns) => Object.assign(accum, ns), {});
+	}
+
 	return {
 		onText: (text) => {
 			if (dom.nodeType === slimdom.Node.DOCUMENT_NODE) {
+				// Do not add text directly to document node (aka. outside document element)
 				return;
 			}
 			dom.appendChild(doc.createTextNode(text));
 		},
 
 		onOpenTag: (node) => {
-			dom = dom.appendChild(doc.createElement(node.name));
+			pushAccumulatedNamespaceInfo(node);
 
+			const prefix = node.name.includes(':') ? node.name.substr(0, node.name.indexOf(':')) : '';
+			if (currentNamespaces[prefix] === undefined) {
+				throw new Error('Namespace prefix "' + prefix + '" not known for element ' + node.name);
+			}
+
+			dom = dom.appendChild(doc.createElementNS(currentNamespaces[prefix], node.name));
+
+			// Set attributes, taking the accumulated namespace information into account
 			Object.keys(node.attributes)
-				.forEach(name => dom.setAttribute(name, node.attributes[name]));
+				.filter(name => name !== 'xmlns' && name.indexOf('xmlns:') !== 0)
+				.forEach(name => {
+					const prefix = name.includes(':') ? name.substr(0, name.indexOf(':')) : null;
+					if (prefix !== null && currentNamespaces[prefix] === undefined) {
+						throw new Error('Namespace prefix "' + prefix + '" not known for attribute ' + name);
+					}
+
+					if (prefix === null) {
+						dom.setAttribute(name, node.attributes[name]);
+					}
+					else {
+						dom.setAttributeNS(currentNamespaces[prefix], name, node.attributes[name]);
+					}
+			});
 		},
 
 		onCloseTag: () => {
-			dom = dom.parentNode
+			dom = dom.parentNode;
+
+			popAccumulatedNamespaceInfo();
 		},
 
 		onProcessingInstruction: (pi) => {
--- index.test.js
+++ index.test.js
@@ -1,12 +1,96 @@
 const { sync } = require('./index');
 
-const xml = `
-<?DOCTYPE html ?>
-<!-- comment -->
-<heck attrName="attrValue">skrrrah<![CDATA[<beep>]]></heck>
-`;
-it('synchronous parser', () => {
-	const doc = sync(xml, true);
-	expect(doc.lastChild.nodeName).toBe('heck');
-	expect(doc.lastChild.getAttribute('attrName')).toBe('attrValue');
+const types = {
+	ELEMENT_NODE: 1,
+	ATTRIBUTE_NODE: 2,
+	TEXT_NODE: 3,
+	CDATA_SECTION_NODE: 4,
+	ENTITY_REFERENCE_NODE: 5,
+	ENTITY_NODE: 6,
+	PROCESSING_INSTRUCTION_NODE: 7,
+	COMMENT_NODE: 8,
+	DOCUMENT_NODE: 9,
+	DOCUMENT_TYPE_NODE: 10,
+	DOCUMENT_FRAGMENT_NODE: 11,
+	NOTATION_NODE: 12
+};
+
+const doc = sync([
+	`<?DOCTYPE html?>`,
+	`<!-- comment -->`,
+	`<root attr="val">`,
+		`<contains-text>text</contains-text>`,
+		`<a:root xmlns:a="http://a" xmlns:b="http://b" a:attr="A" b:attr="B">`,
+			`<a:child xmlns:c="http://a" xmlns:a="http://b" c:attr="A" a:attr="B" />`,
+			`<a:next-sibling a:attr="AAA" />`,
+		`</a:root>`,
+		`<![CDATA[cdata]]>`,
+	`</root>`
+].join(''), true);
+
+it('processing instructions', () => {
+	const subject = doc.firstChild;
+
+	expect(subject.nodeType).toBe(types.PROCESSING_INSTRUCTION_NODE);
+
+	expect(subject.target).toBe('DOCTYPE');
+	expect(subject.nodeValue).toBe('html');
 });
+
+it('comments', () => {
+	const subject = doc.firstChild.nextSibling;
+
+	expect(subject.nodeType).toBe(types.COMMENT_NODE);
+});
+
+it('elements', () => {
+	const subject = doc.documentElement;
+
+	expect(subject.nodeType).toBe(types.ELEMENT_NODE);
+
+	expect(subject.nodeName).toBe('root');
+	expect(subject.localName).toBe('root');
+});
+
+it('text nodes', () => {
+	const subject = doc.documentElement.firstChild.firstChild;
+	expect(subject.nodeType).toBe(types.TEXT_NODE);
+});
+
+it('cdata', () => {
+	const subject = doc.documentElement.firstChild.nextSibling.nextSibling;
+	expect(subject.nodeType).toBe(types.CDATA_SECTION_NODE);
+});
+
+it('attributes', () => {
+	const subject = doc.documentElement;
+
+	expect(subject.attributes[0].nodeType).toBe(types.ATTRIBUTE_NODE);
+
+	expect(subject.getAttribute('attr')).toBe('val');
+});
+
+it('namespaced elements', () => {
+	const subject = doc.documentElement.firstChild.nextSibling;
+
+	expect(subject.nodeType).toBe(types.ELEMENT_NODE);
+
+	expect(subject.nodeName).toBe('a:root');
+	expect(subject.localName).toBe('root');
+});
+
+it('namespaced attributes', () => {
+	const subject = doc.documentElement.firstChild.nextSibling; // <a:root>
+
+	expect(subject.getAttributeNS('http://a', 'attr')).toBe('A');
+	expect(subject.getAttributeNS('http://b', 'attr')).toBe('B');
+
+	// Assert overriding namespace prefixes
+	expect(subject.firstChild.nodeName).toBe('a:child');
+	expect(subject.firstChild.localName).toBe('child');
+	expect(subject.firstChild.getAttributeNS('http://a', 'attr')).toBe('A');
+	expect(subject.firstChild.getAttributeNS('http://b', 'attr')).toBe('B');
+
+	expect(subject.firstChild.nextSibling.getAttributeNS('http://a', 'attr')).toBe('AAA');
+});
+
